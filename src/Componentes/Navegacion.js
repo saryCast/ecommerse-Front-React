394c4c9597946bfb405d303186f79f54d3850a09
saryCast <cast.sara@gmail.com>
2025-01-20 17:33:24 -0300

    arreglando navbar

--- src/Componentes/Navegacion.js
+++ src/Componentes/Navegacion.js
@@ -5,17 +5,17 @@ import Carro from './carro'
 class Navegacion extends React.Component{
     render(){
         return(
-            <Navbar color='ligth' light expand="md">
+            <Navbar color='secondary' light expand="md">
                 <NavbarBrand href='/'>{this.props.titulo}</NavbarBrand>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
                         <NavLink href='/'>Acerca de</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href='/'>React</NavLink>
+                        <NavLink href='/'>Nuestros Productos</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href='/'>JavaScript</NavLink>
+                        <NavLink href='/'>Contactanos</NavLink>
                     </NavItem>
                     <NavItem>
                         <Carro popoverOpen={this.props.popoverOpen} toggleCarro={this.props.toggleCarro} cantidadCarrito={this.props.cantidadCarrito}/>
