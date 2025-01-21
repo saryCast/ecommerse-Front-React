ebe9b8e394a1e9859a3c35afd14c672a392d56be
saryCast <cast.sara@gmail.com>
2025-01-21 10:09:56 -0300

    Updates

---
+++ precache-manifest.96e1ced1275cc2bdb1a8bddb1953d9c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7fd1d2e063d514e0cb752d08123716",
+    "url": "/ecommerse-Front-React/index.html"
+  },
+  {
+    "revision": "3fde7a37181d96ac4f13",
+    "url": "/ecommerse-Front-React/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "25c29d67e73d6a26c728",
+    "url": "/ecommerse-Front-React/static/css/main.92b26f2c.chunk.css"
+  },
+  {
+    "revision": "3fde7a37181d96ac4f13",
+    "url": "/ecommerse-Front-React/static/js/2.e9d66b2d.chunk.js"
+  },
+  {
+    "revision": "25c29d67e73d6a26c728",
+    "url": "/ecommerse-Front-React/static/js/main.94319eb1.chunk.js"
+  },
+  {
+    "revision": "0b89f3dd360de2f5d10d",
+    "url": "/ecommerse-Front-React/static/js/runtime~main.8a7ce022.js"
+  }
+]);
