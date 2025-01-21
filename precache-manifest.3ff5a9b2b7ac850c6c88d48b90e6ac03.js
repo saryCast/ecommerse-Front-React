72f9b00942892cd1372b0ccc2c616555f2aecf25
saryCast <cast.sara@gmail.com>
2025-01-21 16:21:51 -0300

    Updates

--- precache-manifest.3ff5a9b2b7ac850c6c88d48b90e6ac03.js
+++ precache-manifest.3ff5a9b2b7ac850c6c88d48b90e6ac03.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4311bc698f384a37915065ade713eea8",
+    "revision": "41231ede086080168c1e48a1c5c2c18b",
     "url": "/ecommerse-Front-React/index.html"
   },
   {
-    "revision": "8e1c36cae3b0e4cc6332",
+    "revision": "c5ee1932048287535268",
     "url": "/ecommerse-Front-React/static/css/2.dbb74888.chunk.css"
   },
   {
-    "revision": "8be42920834b8e4d9ef5",
+    "revision": "548fb8a0f125ce3df11e",
     "url": "/ecommerse-Front-React/static/css/main.92b26f2c.chunk.css"
   },
   {
-    "revision": "8e1c36cae3b0e4cc6332",
-    "url": "/ecommerse-Front-React/static/js/2.634323d9.chunk.js"
+    "revision": "c5ee1932048287535268",
+    "url": "/ecommerse-Front-React/static/js/2.6b009a93.chunk.js"
   },
   {
-    "revision": "8be42920834b8e4d9ef5",
-    "url": "/ecommerse-Front-React/static/js/main.ed8de85b.chunk.js"
+    "revision": "548fb8a0f125ce3df11e",
+    "url": "/ecommerse-Front-React/static/js/main.d5541dfd.chunk.js"
   },
   {
     "revision": "0b89f3dd360de2f5d10d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecommerse-Front-React/precache-manifest.70521bfb14d53c280e3b0209b18f0ed5.js"
+  "/ecommerse-Front-React/precache-manifest.3ff5a9b2b7ac850c6c88d48b90e6ac03.js"
 );
 
 self.addEventListener('message', (event) => {
